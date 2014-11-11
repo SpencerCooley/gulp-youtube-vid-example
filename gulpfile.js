3b33012eff95e0f0bc12627d14fb45937fe992f3
Spencer Cooley <spencecooley@gmail.com>
2014-11-11 16:26:20 -0600

    basic hello world gulp task

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+
+gulp.task('hello-world', function() {
+  console.log("hello world")
+  console.log("hello world")
+  console.log("hello world")
+  console.log("hello world")
+  console.log("hello world")
+  console.log("hello world")
+  console.log("hello world")
+});
+
