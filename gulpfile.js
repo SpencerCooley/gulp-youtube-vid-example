7c09d5baf761996dd0d58997a99b22e4d51e79b5
Spencer Cooley <spencecooley@gmail.com>
2014-11-11 17:19:55 -0600

    a simple example of converting scss to css. Also watches for file changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,29 @@
+
+
 var gulp = require('gulp');
+
+//look at the documentation for this plugin at https://github.com/sindresorhus/gulp-ruby-sass
+var sass = require('gulp-ruby-sass');
+
+
+
+gulp.task('process-scss', function () {
+    return gulp.src('./assets/scss/main-style.scss')
+    	.pipe(sass())
+        .on('error', function (err) { console.log(err.message); })
+        .pipe(gulp.dest('result/css'));
+});
+
+gulp.task('watch', function() {
+  
+  // watch scss files for changes
+  //then runs the process-scss change.
+  gulp.watch('./assets/scss/**/*.scss', function() {
+  	gulp.run('process-scss');
+  });
+
+
+});
 
 gulp.task('hello-world', function() {
   console.log("hello world")
@@ -9,4 +34,8 @@ gulp.task('hello-world', function() {
   console.log("hello world")
   console.log("hello world")
 });
+
+
+
+
 
