6ef7f51ced699607513efefce11b8010bcd46630
Spencer Cooley <spencecooley@gmail.com>
2014-11-11 17:59:32 -0600

    added a task to the js branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
 
+
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+
+
+gulp.task('process-js', function() {
+  //concatenates a number of javascript files, then runs uglify and putst the final result  in ./result/js/
+  gulp.src(js_order)
+    .pipe(concat('app.js'))
+    .pipe(uglify({"mangle":true}))
+    .pipe(gulp.dest('./result/js/'))
+});
+
+
+
+
+
 gulp.task('hello-world', function() {
   console.log("hello world")
   console.log("hello world")
